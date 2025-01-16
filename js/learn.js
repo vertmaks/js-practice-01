392037045c7b0ff3d5e33f2eecb2d848b103ac49
Maksym Veretnov <maxvertnov@gmail.com>
2025-01-16 23:49:19 +0100

    add html example markup, add exmpl sprite, add js code

--- js/learn.js
+++ js/learn.js
@@ -1,3 +1,27 @@
-if (condition) {
-  // код, який виконується, якщо умова (condition) істинна
+// if (condition) {
+//   // код, який виконується, якщо умова (condition) істинна
+// }
+
+let price = 0;
+const subscription = "pro";
+
+if (subscription === "pro") {
+  price = 100;
 }
+
+console.log("price:", price);
+
+let price2 = 0;
+const subscription2 = "free";
+
+if (subscription2 === "pro") {
+  price = 100;
+}
+
+console.log("price2:", price2);
+
+// if (condition) {
+//   // код, який виконується, якщо умова істинна
+// } else {
+//   // код, який виконується, якщо умова хибна
+// }
